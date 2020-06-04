7ffdbb7e74c030ae56a16365fdf927979bd4b493
Karem Ceron <karemiceron@gmail.com>
2020-06-04 21:00:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index do |dwarf, index + 1|
+  array.each_with_index do |dwarf, index = index + 1|
     puts "#{index}. #{dwarf}"
   end
 end
