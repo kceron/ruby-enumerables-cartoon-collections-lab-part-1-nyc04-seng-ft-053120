b129d69e5df9b7b0173b51196d79f8c33b933117
Karem Ceron <karemiceron@gmail.com>
2020-06-04 20:58:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,14 +5,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each_with_index { |dwarf|, index|
-  
-  # Use `each_with_index` to enumerate over the provided array
-  #
-  # Print a numbered list of each element
+  array.each_with_index do |dwarf|, index|
+    puts "#{index + 1}. #{dwarf|}"
+  end
 end
-
-%w(cat dog wombat).each_with_index { |item, index|
-  hash[item] = index
-}
-hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
